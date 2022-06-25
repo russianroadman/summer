beb6c565c8cfd516a6b4662dacfae6b714832444
russianroadman <realgar2@gmail.com>
2022-06-26 00:48:18 +0800

    Initial commit

---
+++ src/test/java/com/tanto/summer/SummerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanto.summer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
